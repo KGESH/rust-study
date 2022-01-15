83056671cd8a5e39ff7186e9524a61be45dcdb2d
KGESH <baram789456@gmail.com>
2022-01-15 15:54:30 +0900

    Start Rust CLI Prject '

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::env;
+use std::fs::File;
+use std::io::Read;
+
+
 fn main() {
-    println!("Hello, Rust!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+    println!("command {}", query);
+    println!("file name {}", filename);
+
+    let mut file = File::open(filename)
+        .expect("File not found!");
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)
+        .expect("Something went wrong reading to string!");
+
+    println!("With Text : \n{}", contents);
+
 }
